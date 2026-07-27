28f5740ae63931193045a68f26e5e1eabe06219c
siddhant8189-stack <siddhant8189@gmail.com>
2026-07-27 15:39:20 +0530

    index.js is added

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Siddhant chauhan");
+// console.log("Siddhant chauhan");
+// function sum(a,b){
+//     const sum=a+b;
+//     return Math.sqrt(sum);
+// }
+
+
+// console.log(sum(15+16));
+
+let a=34;
+if(a>20){
+    let a=45;
+    console.log("a inside a="+a)
+}
+console.log("a inside a=" + a)
